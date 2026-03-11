533352f1b97d221e96033900a9fb9daa62710ebd
hchashchina <hanna@terenbro.com>
2026-03-11 12:16:41 +0200

    Add initial Postgres migration

--- prisma/migrations/20260309155914_add_review/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Review" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "productId" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "rating" INTEGER NOT NULL,
-    "text" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
